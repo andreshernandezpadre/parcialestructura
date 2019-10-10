d73f1df389e23550624f4edfbfe3d7fbd9768028
USUARIO <USUARIO@DESKTOP-PHOHDN3>
2019-10-10 11:39:15 -0500

    cambio

--- src/parcialestructura/ParcialEstructura.java
+++ src/parcialestructura/ParcialEstructura.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 
 public class ParcialEstructura {
     public static void main (String []  args){
-        
+        int prueba=0;
       
          QuizEstructura obj = new QuizEstructura();
         int i=1;
